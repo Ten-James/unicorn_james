75367fa20e4bca7350fcbe5c47a7995a8c03827d
Ten-James <hexaplay2@gmail.com>
2021-05-24 13:44:30 +0200

    navbar commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import "./styles/App.css";
 
+import React from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import Products from "./componens/Products";
+import Contact from "./componens/Contact";
+import About from "./componens/About";
+
 function App() {
   return (
-    <div className="App">
-      <div className="header">
-        <h1>Ahoj, vítej v mojí aplikaci</h1>
+    <Router>
+      <div className="App">
+        <nav className="header">
+          <h1>SPACE SHOP</h1>
+          <ul>
+            <li>
+              <Link to="/">Products</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/kosik">Kosik</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/" component={Products} />
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
